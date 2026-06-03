4812abb430d3c1afbfc2026d3ef8f79f96e797f7
LHF <971605209@qq.com>
2026-06-03 18:25:34 +0800

    配置GitHub Pages部署路径

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import { VantResolver } from '@vant/auto-import-resolver'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/my-study-log/',
   plugins: [
     vue(),
     AutoImport({
